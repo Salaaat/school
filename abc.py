d34547018114fe7ce6978ed50fe2aac7ff514348
Salaaat <105119162+Salaaat@users.noreply.github.com>
2023-04-24 11:08:26 +0200

    24 April
    input - string len max 15
    for kazde pismeno print tolikrat kolikaty je v abecede

---
+++ abc.py
@@ -0,0 +1,12 @@
+def printer(word):
+    if len(word) > 15:
+        return "max 15 pls"
+    abc = []
+    for i in "abcdefghijklmnopqrstuvwxyz":
+        abc.append(i)
+    abc = tuple(abc)
+    for i in word:
+        print(i*abc.index(i))
+
+while True:
+    printer(input())
